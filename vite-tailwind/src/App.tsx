e90ca2f6cad272e238efcda77610f1eee3902e2a
Taraslysak <lysak.ipr@gmail.com>
2023-02-22 16:43:00 +0200

    added vite

---
+++ vite-tailwind/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <p className="text-pink-400">HELLO WORLD</p>
+    </div>
+  );
+}
+
+export default App;
